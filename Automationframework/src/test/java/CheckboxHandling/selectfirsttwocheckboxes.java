1682606eca3e325b6110d4f9b2168d345d1cc79d
Dilip491 <delip5491@gmail.com>
2024-02-09 20:58:32 +0530

    Added the checkbox changes

---
+++ Automationframework/src/test/java/CheckboxHandling/selectfirsttwocheckboxes.java
@@ -0,0 +1,28 @@
+package CheckboxHandling;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class selectfirsttwocheckboxes {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+
+        List<WebElement> checkboxlist=driver.findElements(By.xpath("//input[@type='checkbox' and @class='cb1-element']"));
+        System.out.println(checkboxlist.size());
+
+        int cbsize=checkboxlist.size();
+        for(int i=0;i<cbsize;i++){
+            if(i<2){
+                checkboxlist.get(i).click();
+            }
+
+        }
+    }
+}
---
+++ Automationframework/src/test/java/CheckboxHandling/selectspeccificcheckbox.java
@@ -0,0 +1,27 @@
+package CheckboxHandling;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class selectspeccificcheckbox {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+        List<WebElement> checkboxlist=driver.findElements(By.xpath("//label[contains(.,'Option')]"));
+        System.out.println(checkboxlist.size());
+        for (WebElement checkbox:checkboxlist){
+            if(checkbox.getText().equals("Option 2")||checkbox.getText().equals("Option 4")){
+                checkbox.click();
+                System.out.println(checkbox.getText());
+            }
+        }
+
+    }
+}
